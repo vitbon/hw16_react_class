bdf1d61a8df0354e5bfbc0103e211214ffb1681c
Vitalii <vit.bondarchuk@gmail.com>
2020-09-08 14:15:29 +0300

    Updates

---
+++ precache-manifest.0acdbf87682ce0c4723156af8fd39d0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25414c1cf6edce05a91b5580952a68e",
+    "url": "/hw16_react_class/index.html"
+  },
+  {
+    "revision": "441c93c0a6c9c2b390e8",
+    "url": "/hw16_react_class/static/css/main.ea7922ce.chunk.css"
+  },
+  {
+    "revision": "8413ff69592eafd1b948",
+    "url": "/hw16_react_class/static/js/2.3461c589.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw16_react_class/static/js/2.3461c589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441c93c0a6c9c2b390e8",
+    "url": "/hw16_react_class/static/js/main.292e650b.chunk.js"
+  },
+  {
+    "revision": "a0424fa706bb45cd1a30",
+    "url": "/hw16_react_class/static/js/runtime-main.ff26b51d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hw16_react_class/static/media/logo.5d5d9eef.svg"
+  }
+]);
