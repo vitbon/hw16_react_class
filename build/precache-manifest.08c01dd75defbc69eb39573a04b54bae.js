caa1862f11acb04e40c2804be09deb64ab98612a
Vitalii <vit.bondarchuk@gmail.com>
2020-09-08 14:01:38 +0300

    HW16 Class Pomponents

---
+++ build/precache-manifest.08c01dd75defbc69eb39573a04b54bae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbedbbf918830376001db32426d0a3be",
+    "url": "/hw16_react_class/index.html"
+  },
+  {
+    "revision": "b39cd016f607545e0758",
+    "url": "/hw16_react_class/static/css/main.ea7922ce.chunk.css"
+  },
+  {
+    "revision": "216bd9891e769bec0771",
+    "url": "/hw16_react_class/static/js/2.3c1cc815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw16_react_class/static/js/2.3c1cc815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39cd016f607545e0758",
+    "url": "/hw16_react_class/static/js/main.57351ed8.chunk.js"
+  },
+  {
+    "revision": "a0424fa706bb45cd1a30",
+    "url": "/hw16_react_class/static/js/runtime-main.ff26b51d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hw16_react_class/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6fd50452f3aab1ddd87f240299bd15f",
+    "url": "/hw16_react_class/static/media/pictol.d6fd5045.jpg"
+  }
+]);
